682a96b7ee555fa9d3f81cbef56f17d6d0065080
Viraj <desaiviraj28@gmail.com>
2020-05-29 01:49:02 +0530

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,48 +16,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
-
-app.all('/dishes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-/*
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
- res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
- 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-*/
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);
@@ -70,13 +32,3 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-/*
-var path = require("path"); 
-  
-// Methods to display directory 
-console.log("__dirname:    ", __dirname); 
-console.log("process.cwd() : ", process.cwd()); 
-console.log("./ : ", path.resolve("./")); 
-console.log("filename: ", __filename); 
-*/
