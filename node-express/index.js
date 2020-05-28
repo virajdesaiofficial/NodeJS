b014862da6f6a555d8a21aab31e761698d1ae5ec
Viraj <desaiviraj28@gmail.com>
2020-05-28 18:38:13 +0530

    HTTP server using Express and morgan middleware

---
+++ node-express/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+/*
+var path = require("path"); 
+  
+// Methods to display directory 
+console.log("__dirname:    ", __dirname); 
+console.log("process.cwd() : ", process.cwd()); 
+console.log("./ : ", path.resolve("./")); 
+console.log("filename: ", __filename); 
+*/
